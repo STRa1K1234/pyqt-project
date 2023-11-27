bbabfdf0aa0fdde030c701d07f2759df7f687f49
STRa1K1234 <139379133+STRa1K1234@users.noreply.github.com>
2023-11-28 00:53:27 +0300

    Рабочая программа. 1 TODO

--- pyqt main.py
+++ pyqt main.py
@@ -1,40 +1,54 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
-from PyQt5.QtGui import QFont
 from PyQt5.QtCore import QTimer, Qt, QEvent
 
-
 class SpeedTypingTest(QWidget):
-    def __init__(self):
-        super().__init__()
+    def startTest(self):
+        # Устанавливаем фокус на поле ввода и очищаем его
+        self.input.setFocus()
+        self.input.clear()
+        # Сбрасываем время
+        self.timeElapsed = 0
+        # Устанавливаем соединение с методом updateTime при срабатывании таймера и запускаем его
+        self.timer.timeout.connect(self.updateTime)
+        self.timer.start(1000)  # Таймер срабатывает каждую секунду
+        self.stopped = False  # Устанавливаем флаг остановки в False
+
+    def init(self):
+        super().init()
         self.initUI()
 
     def initUI(self):
+        # Настройка интерфейса
         self.setGeometry(100, 100, 400, 200)
         self.setWindowTitle('Speed Typing Test')
 
-        self.label = QLabel('Type the following sentence and press "enter":', self)
-        self.label.move(20, 20)
+        self.label = QLabel('Type the following sentence:', self)
+        self.label.move(20, 20) # установка условий
 
         self.sentence = QLabel('The quick brown fox jumps over the lazy dog', self)
-        self.sentence.setGeometry(20, 40, 360, 20)
+        self.sentence.setGeometry(20, 40, 360, 20)  # установка строки
 
         self.input = QLineEdit(self)
         self.input.setGeometry(20, 70, 360, 20)
-        self.input.installEventFilter(self)
+        self.input.installEventFilter(self)  # создание окна
 
         self.startButton = QPushButton('Start', self)
         self.startButton.setGeometry(20, 100, 80, 30)
-        self.startButton.clicked.connect(self.startTest)
+        self.startButton.clicked.connect(self.startTest)  # Привязываем нажатие кнопки "Start" к методу startTest
 
         self.timerLabel = QLabel('Time: 0 seconds', self)
-        self.timerLabel.setGeometry(300, 100, 100, 30)
+        self.timerLabel.setGeometry(300, 100, 100, 30)  # установка таймера
 
-        self.timer = QTimer()
-        self.timeElapsed = 0
-        self.stopped = False
+        self.speedLabel = QLabel('Speed: 0 characters/minute', self)
+        self.speedLabel.setGeometry(20, 120, 150, 30)   # установка скорости
+
+        self.timer = QTimer()  # Создаем таймер для отслеживания времени
+        self.timeElapsed = 0  # Переменная для отслеживания прошедшего времени
+        self.stopped = False  # Флаг остановки для отслеживания завершения теста
 
     def startTest(self):
+        # Начало теста
         self.input.setFocus()
         self.input.clear()
         self.timeElapsed = 0
@@ -43,13 +57,12 @@ class SpeedTypingTest(QWidget):
         self.stopped = False
 
     def eventFilter(self, obj, event):
-        if obj is self.input and event.type() == QEvent.KeyPress and event.key() in (
-        Qt.Key_Return, Qt.Key_Enter) and not self.stopped:
+        # Фильтр событий для поля ввода
+        if obj is self.input and event.type() == QEvent.KeyPress and event.key() in (Qt.Key_Return, Qt.Key_Enter) and not self.stopped:
             self.timer.stop()
             self.stopped = True
             accuracy = self.calculateAccuracy()
             self.showResult(accuracy)
-
         return super().eventFilter(obj, event)
 
     def updateTime(self):
@@ -57,25 +70,27 @@ class SpeedTypingTest(QWidget):
         self.timerLabel.setText(f'Time: {self.timeElapsed} seconds')
 
     def calculateAccuracy(self):
+        # Метод расчета точности ввода
         sentence = self.sentence.text()
         userInput = self.input.text()
         correctCharacters = sum(1 for i, j in zip(sentence, userInput) if i == j)
         accuracy = (correctCharacters / len(sentence)) * 100
+        speed = len(userInput) / self.timeElapsed * 60
+        self.speedLabel.setText(f'Speed: {speed:.2f} characters/minute')
         return accuracy
 
     def showResult(self, accuracy):
+        # Метод отображения результата теста
         resultLabel = QLabel(f'Accuracy: {accuracy:.2f}% \nTime: {self.timeElapsed} seconds', self)
-        resultLabel.move(20, 140)
-        resultLabel.setFont(QFont('Arial', 10))
+        resultLabel.move(20, 143)
         resultLabel.show()
 
 
+# запуск программы
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = SpeedTypingTest()
     ex.show()
     sys.exit(app.exec_())
 
-# TODO исправить наложение текста при повторном нажатии на старт
 # TODO добавить датабазу
-# TODO комментарии
