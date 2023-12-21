ec61bb1491df9a295f9be8407d45245a2c73f1ca
STRa1K1234 <139379133+STRa1K1234@users.noreply.github.com>
2023-12-22 01:12:35 +0300

    final version

--- pyqt main.py
+++ pyqt main.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QDesktopWidget
 from PyQt5.QtCore import QTimer, Qt, QEvent
+import random
+import sqlite3
 
 class SpeedTypingTest(QWidget):
     def startTest(self):
@@ -20,13 +22,14 @@ class SpeedTypingTest(QWidget):
 
     def initUI(self):
         # Настройка интерфейса
-        self.setGeometry(100, 100, 400, 200)
+        self.setGeometry(100, 100, 750, 200)
+        self.setStyleSheet("background-color: #C3E1C9;")
         self.setWindowTitle('Speed Typing Test')
 
         self.label = QLabel('Type the following sentence:', self)
         self.label.move(20, 20) # установка условий
 
-        self.sentence = QLabel('The quick brown fox jumps over the lazy dog', self)
+        self.sentence = QLabel('', self)
         self.sentence.setGeometry(20, 40, 360, 20)  # установка строки
 
         self.input = QLineEdit(self)
@@ -38,14 +41,38 @@ class SpeedTypingTest(QWidget):
         self.startButton.clicked.connect(self.startTest)  # Привязываем нажатие кнопки "Start" к методу startTest
 
         self.timerLabel = QLabel('Time: 0 seconds', self)
-        self.timerLabel.setGeometry(300, 100, 100, 30)  # установка таймера
+        self.timerLabel.setGeometry(300, 100, 200, 30)  # установка таймера
 
         self.speedLabel = QLabel('Speed: 0 characters/minute', self)
-        self.speedLabel.setGeometry(20, 120, 150, 30)   # установка скорости
+        self.speedLabel.setGeometry(20, 120, 200, 30)   # установка скорости
 
         self.timer = QTimer()  # Создаем таймер для отслеживания времени
         self.timeElapsed = 0  # Переменная для отслеживания прошедшего времени
         self.stopped = False  # Флаг остановки для отслеживания завершения теста
+        self.center()  # вызов нового метода для центрирования окна
+        self.loadRandomSentence()  # загружаем случайную строку из базы данных
+        self.sentence.setWordWrap(True)  # позволяет переносить текст внутри Label
+        self.sentence.setFixedSize(400, 30)  # установка фиксированного размера для Label
+
+    def loadRandomSentence(self):
+        # Подключение к базе данных
+        conn = sqlite3.connect('new_database.db')
+        cursor = conn.cursor()
+
+        # Выбор случайной строки из базы данных
+        cursor.execute("SELECT line FROM lines ORDER BY RANDOM() LIMIT 1")
+        result = cursor.fetchone()
+        if result:
+            self.sentence.setText(result[0])
+
+        conn.close()
+
+    def center(self):
+        # Метод для центрирования окна
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
     def startTest(self):
         # Начало теста
@@ -93,4 +120,3 @@ if __name__ == '__main__':
     ex.show()
     sys.exit(app.exec_())
 
-# TODO добавить датабазу
